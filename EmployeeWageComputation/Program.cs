20a5fb917b5a3a18e2c866435ebb122fc1927615
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-07 11:32:30 +0530

    [Sunny] Add: EmployeeAttendence Implementation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeWageComputation;
+WageComputation computation = new WageComputation();
+computation.EmployeeAttendence();
