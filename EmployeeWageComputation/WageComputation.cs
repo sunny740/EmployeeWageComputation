7dcfadba8b9a75b7e01ab15719ded43ca813f0e4
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-07 17:07:58 +0530

    [Sunny] Add: EmpWageCaseStatement Added

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -11,32 +11,32 @@ namespace EmployeeWageComputation
         const int IS_PRESENT = 0;
 
         //Conatant 
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         //Variable
-        public void EmpWageParttime()
+        public void EmpWageCaculate()
         {
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
         }
-
     }
 }
