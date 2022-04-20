8e18e347eee731bfb7fe6ccd98dad1e91a4e17de
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-20 15:03:43 +0530

    [Sunny] Add : Calculating Employee salary

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -2,4 +2,6 @@
 using EmployeeWageComputation;
 WageComputation computation = new WageComputation();
 //computation.EmployeeAttendence();
-computation.EmpWage();
+computation.EmpWage("Google", 20, 2, 10);
+computation.EmpWage("Microsoft", 10, 4, 20);
+computation.EmpWage("Deloitte", 15, 3, 15);
--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -8,48 +8,41 @@ namespace EmployeeWageComputation
 {
     public class WageComputation
     {
-        const int Is_Full_Time = 1;
-        const int Is_Part_Time = 2;
-        const int employeeWorkingHrs = 20;
-        const int employeeWorkingDays = 20;
-        public void EmpWage()
+        //constance value
+        const int PART_TIME_EMP = 1,
+        FULL_TIME_EMP = 2,
+        EMP_FULL_DAY_WRKING_HR = 8,
+        EMP_PART_TIME_WRKING_HR = 4;
+
+        //variable values
+        int empHrs = 0, totalEmpSalary = 0, totalEmpHrs = 0, day = 0;
+
+        public void EmpWage(string company_Name, int EMP_WAGE_PR_HR, int MAX_WORKING_HR, int EMP_WORKING_Days_PR_MONTH)
         {
-            int salary = 0;
-            int totalSalary = 0;
-            int empHrs = 0;
-            int sum = 0;
-
-            for (int i = 1; i <= 20; i++)
+            while (day <= EMP_WORKING_Days_PR_MONTH && empHrs <= MAX_WORKING_HR)
             {
-
-
                 Random random = new Random();
-                int check = random.Next(0, 3);
+                int empCheck = random.Next(0, 3);
 
-                switch (check)
+                switch (empCheck)
                 {
-                    case Is_Full_Time:
-                        empHrs = 8;
+                    case FULL_TIME_EMP:
+
+                        empHrs += EMP_FULL_DAY_WRKING_HR;
                         break;
-                    case Is_Part_Time:
-                        empHrs = 4;
+                    case PART_TIME_EMP:
+
+                        empHrs += EMP_PART_TIME_WRKING_HR;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                salary = empHrs * employeeWorkingHrs;
-                Console.WriteLine("Day" + i + " " + salary);
-                totalSalary = totalSalary + salary;
-                if (empHrs <= 100)
-                {
-                    sum = sum + empHrs;
-                }
+                day++;
             }
-
-            Console.WriteLine("Total Salary of 20days" + " =" + totalSalary);
-            Console.WriteLine("Total Working Hrs is" + " " + sum);
-
+            totalEmpSalary = empHrs * EMP_WAGE_PR_HR;
+            Console.WriteLine("Company : " + company_Name + ", Emp Working Hour : " + EMP_WAGE_PR_HR + ", Maximum working Hour : " + MAX_WORKING_HR + ", Total Days In Month : " + EMP_WORKING_Days_PR_MONTH);
+            Console.WriteLine("One month Employee Salary is :" + totalEmpSalary);
         }
     }
 }
